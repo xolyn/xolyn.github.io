1dc6ba1a1a110d6adc45f060bdb79b9be80f119d
Lingyu Zhou <zly@dr.com>
2024-06-02 21:52:54 -0400

    new

--- font.py
+++ font.py
@@ -5,5 +5,5 @@ for i in fl:
     ct+='''@font-face {font-family:'''+i.split(".")[0]+'''; src:url("fonts/'''+i+'''");}\n'''
 with open('font.css', 'w') as file:
     file.write(ct)
-    print("complied successfully.")
+    print("compiled successfully.")
 # print(ct)
