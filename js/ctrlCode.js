1ecc0691a38d1042cd1e3952e4d5e98e732d2d8e
aqmn <147225958+aqmn@users.noreply.github.com>
2025-08-07 01:36:48 +0800

    changes

---
+++ js/ctrlCode.js
@@ -0,0 +1,25 @@
+window.addEventListener('DOMContentLoaded', function() {
+  const selectors = ['pre', 'samp', 'code'];
+  selectors.forEach(sel => {
+    document.querySelectorAll(sel).forEach(el => {
+      const btn = document.createElement('button');
+      btn.innerHTML = '&#x2398;';
+      btn.style.position = 'absolute';
+      btn.style.top = '1rem';
+      btn.style.right = '1rem';
+      btn.style.border = '1px solid #ddd';
+      btn.style.backgroundColor = '1px solid #ccc';
+      btn.style.borderRadius = '20%';
+      btn.style.color = 'inherit';
+      btn.style.zIndex = '10';
+      if (getComputedStyle(el).position === 'static') {
+        el.style.position = 'relative';
+      }
+      btn.onclick = function(e) {
+        e.stopPropagation();navigator.clipboard.writeText(el.innerText);
+        btn.innerHTML = '&check;'; setTimeout(()=>btn.innerHTML = '&#x2398;', 500);
+      };
+      el.appendChild(btn);
+    });
+  });
+});
